b3b6fdb3d1b95e3dea0725cb48c0a208672a4097
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-05-26 16:51:55 +0600

    `Created the Build File For Deployment`

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,17 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-import Root from "../layout/Root";
-import Home from "../pages/Home";
-import Course from "../pages/Course";
-import Training from "../pages/Trainer";
-import Games from "../pages/Games";
-import Gallery from "../pages/Gallery";
-import Blog from "../pages/Blog";
-import Contact from "../pages/Contact";
 import AdmissionForm from "../components/AdmissionForm";
 import LoginForm from "../components/LoginForm";
-import OurWork from "../components/OurWork";
-import PhotoGallery from "../components/PhotoGAllery";
-import VideoGallery from "../components/VideoGAllery";
+import PhotoGallery from "../components/PhotoGallery";
+import VideoGallery from "../components/VideoGallery";
+import Root from "../layout/Root";
+import Blog from "../pages/Blog";
+import Contact from "../pages/Contact";
+import Course from "../pages/Course";
+import Games from "../pages/Games";
+import Home from "../pages/Home";
+import Training from "../pages/Trainer";
 
 export const router = createBrowserRouter([
   {
@@ -37,15 +35,11 @@ export const router = createBrowserRouter([
       },
       {
         path: "/photosGallery",
-        element: <PhotoGallery></PhotoGallery>,
+        element: <PhotoGallery />,
       },
       {
         path: "/VideoGallery",
-        element: <VideoGallery></VideoGallery>
-      },
-      {
-        path: "/photosGallery",
-        element: <OurWork></OurWork>,
+        element: <VideoGallery />,
       },
       {
         path: "/blog",
